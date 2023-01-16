dc959ecb96de53b4e8f1e066c924aaade2b6b257
Semin Park <spark9625@gmail.com>
2023-01-16 12:34:01 +0800

    Hop MVP

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -291,6 +291,8 @@ pub struct Config {
     pub insert_final_newline: bool,
     /// Enables smart tab
     pub smart_tab: Option<SmartTabConfig>,
+    // Enables jump mode
+    pub jump_mode: JumpModeConfig,
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Eq, PartialOrd, Ord)]
@@ -305,6 +307,27 @@ impl Default for SmartTabConfig {
         SmartTabConfig {
             enable: true,
             supersede_menu: false,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(default, rename_all = "kebab-case", deny_unknown_fields)]
+pub struct JumpModeConfig {
+    /// Whether or not to dim the view when in jump mode. Defaults to `true`.
+    pub dim_during_jump: bool,
+    /// How many characters the user should type before labelling the targets.
+    pub num_chars_before_label: u8,
+    /// Keys used in labels. Should be convertible to u8
+    pub jump_keys: String,
+}
+
+impl Default for JumpModeConfig {
+    fn default() -> Self {
+        JumpModeConfig {
+            dim_during_jump: true,
+            num_chars_before_label: 1,
+            jump_keys: String::from("jwetovxqpdygfblzhckisuranm"),
         }
     }
 }
@@ -846,6 +869,7 @@ impl Default for Config {
             default_line_ending: LineEndingConfig::default(),
             insert_final_newline: true,
             smart_tab: Some(SmartTabConfig::default()),
+            jump_mode: JumpModeConfig::default(),
         }
     }
 }
